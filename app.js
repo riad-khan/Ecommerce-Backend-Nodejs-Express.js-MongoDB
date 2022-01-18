26ce90eb41f06270251a6b761cc6d4d66a1e0374
riad-khan <rhkhan2367@gmail.com>
2022-01-18 23:32:14 +0600

    refactoring Codes

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
 require('express-async-errors');
 const express = require('express');
 const app = express();
-const cors = require('cors');
-const userRouter = require('./routers/userRouter');
 const error = require('./middlewares/error');
-const categoryRouter = require('./routers/categoryRouter');
-const productRouters = require('./routers/productRouter')
-
-app.use(express.json());
-app.use(cors());
-app.use('/api/user',userRouter);
-app.use('/api/category',categoryRouter);
-app.use('/api/product',productRouters)
-
-
+require('./middlewares')(app);
+require('./routes')(app);
 app.use(error)
 
 module.exports = app ;
